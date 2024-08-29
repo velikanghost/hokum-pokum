1d90fbfb438b3cff0b83267fe289175ec6955444
velikanghost <velikanghost@yahoo.com>
2024-08-29 10:12:30 +0100

    ..

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { CheckoutComponent } from "../components/CheckoutComponent";
+import React from 'react'
+import { CheckoutComponent } from '../components/CheckoutComponent'
 
 const Page = () => {
   return (
     <main className="m-3">
       <CheckoutComponent />
     </main>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
