eedfe613578074169b46d1b0164bf3dc6058941d
velikanghost <velikanghost@yahoo.com>
2024-08-29 19:34:18 +0100

    fixed responsiveness

--- app/components/Homepage/HeroSection.tsx
+++ app/components/Homepage/HeroSection.tsx
@@ -8,18 +8,18 @@ export const HeroSection = () => {
     <section className="relative h-[100dvh] md:h-[88dvh]">
       <div className="absolute top-0 w-full h-full bg-secondary-foreground opacity-5 rounded-[4px] md:rounded-xl"></div>
       <div className="h-full w-full rounded-[4px] md:rounded-xl"></div>
-      <div className="w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="flex justify-between items-center gap-20">
-          <div className="flex-1">
-            <h1 className="text-secondary drop-shadow-sm text-[45px] md:text-[60px] leading-[120%] font-headings font-bold mb-6">
+      <div className="w-full absolute top-[70%] lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-20 pb-[60px] lg:pb-0">
+          <div className="text-center lg:text-start flex-1">
+            <h1 className="text-secondary drop-shadow-sm text-[40px] md:text-[65px] leading-[120%] font-headings font-bold mb-6">
               Instant Transactions <br /> One Click, Any Chain.
             </h1>
-            <p className=" text-secondary drop-shadow-xl text-xl md:text-2xl mb-10 font-medium justify-center leading-[155%]">
+            <p className="text-secondary drop-shadow-xl text-xl md:text-2xl mb-10 font-medium justify-center leading-[155%]">
               Experience Heekowave, A payment solution powered by Monad Layer-1
               blockchain, providing fast one-click transactions regardless of
               source and destination chains.
             </p>
-            <div className="flex gap-4 md:gap-6">
+            <div className="flex justify-center items-center lg:justify-start gap-4 md:gap-6">
               <Button
                 variant="default"
                 className=" btn-primary text-primary-foreground"
