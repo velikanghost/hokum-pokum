bbe8e0b1a5556872a3629a36dd09cd12c77110f3
velikanghost <velikanghost@yahoo.com>
2024-08-31 06:54:18 +0100

    fixed deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`  bg-[#1F2026]`}>
+      <body className={`bg-[#1F2026]`}>
         <Navbar />
         {children}
       </body>
