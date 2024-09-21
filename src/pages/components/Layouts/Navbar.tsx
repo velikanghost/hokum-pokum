f8349d4fae850383e6a0ade4aec0acbcad73b9fe
velikanghost <velikanghost@yahoo.com>
2024-09-21 16:02:23 +0100

    added redeem page: wip

--- src/pages/components/Layouts/Navbar.tsx
+++ src/pages/components/Layouts/Navbar.tsx
@@ -10,6 +10,7 @@ import { NavigationMenuLink } from '@radix-ui/react-navigation-menu'
 import { Button } from '@/components/ui/button'
 import { BsJournalText } from 'react-icons/bs'
 import { BiHomeSmile } from 'react-icons/bi'
+import { MdRedeem } from 'react-icons/md'
 
 const Navbar = () => {
   const [showSidebar, setShowSidebar] = useState<boolean>(false)
@@ -18,6 +19,11 @@ const Navbar = () => {
       icon: <BiHomeSmile size={20} />,
       name: 'Home',
       link: '/',
+    },
+    {
+      icon: <MdRedeem size={18} />,
+      name: 'Redeem',
+      link: '/redeem',
     },
     {
       icon: <RiTwitterXLine size={18} />,
@@ -34,8 +40,8 @@ const Navbar = () => {
   return (
     <>
       <NavigationMenu className="mx-auto navigation-menu">
-        <NavigationMenuList className="container nav-container w-screen justify-between items-center pt-2">
-          <NavigationMenuItem className="text-2xl text-primary-foreground py-4 font-headings font-semibold">
+        <NavigationMenuList className="container items-center justify-between w-screen pt-2 nav-container">
+          <NavigationMenuItem className="py-4 text-2xl font-semibold text-primary-foreground font-headings">
             Heekowave
           </NavigationMenuItem>
 
@@ -53,7 +59,7 @@ const Navbar = () => {
           <div className="nav-menu-items text-primary-foreground">
             {menuItems.map((item, i) => (
               <NavigationMenuLink
-                className="flex justify-center items-center gap-2 hover:animate-bounce h-full py-4 text-base"
+                className="flex items-center justify-center h-full gap-2 py-4 text-base hover:animate-bounce"
                 href={item.link}
                 key={i}
                 target={item.name === 'Home' ? '_parent' : '_blank'}
